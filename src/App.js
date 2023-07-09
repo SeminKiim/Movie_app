2d21a8ffb561c9239dba083f8e687a5437be6355
SeminKiim <seminik62@gmail.com>
2023-07-10 00:00:25 +0900

    7/9

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import './App.css';
 
-function Potato() {
-  return (
-    <h3>This is Potato</h3>
-  );
-}
 
 function Food({ name, picture }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <p>I like {picture}</p>
+      <img src={picture} />
     </div>
   );
 }
 
-const foodLike = [
+const foodILike = [
   {
     name: "Kimchi",
-    image: "kimchi.png"
+    image: "https://cookingchew.com/wp-content/uploads/2020/05/What-Is-Kimchi-CO180-FE.jpg"
   },
   {
     name: "coffee",
-    image: "coffee.png"
+    image: "https://cdn.zmescience.com/wp-content/uploads/2018/04/Cappuccino_at_Sightglass_Coffee-1.jpg"
   },
   {
     name: "Bread",
-    image: "bread.png"
+    image: "https://www.thespruceeats.com/thmb/9n3YM4RVKSWyUWQ0JCl7BwHrl1U=/4288x2848/filters:fill(auto,1)/loaf-of-bread-182835505-58a7008c5f9b58a3c91c9a14.jpg"
   }
 ];
 
@@ -35,10 +30,10 @@ function renderFood(dish) {
 }
 
 function App() {
+  console.log(foodILike.map(renderFood));
   return (
     <div>
-      <h1>Hello</h1>
-      {foodLike.map(renderFood)}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
