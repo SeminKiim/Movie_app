a3ff21160756b15f39ea380341dd205e90d97769
SeminKiim <seminik62@gmail.com>
2023-07-12 13:57:29 +0900

    7/12 생애주기

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import './App.css';
-import PropTypes from 'prop-types';
+import React from 'react'
 
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://cookingchew.com/wp-content/uploads/2020/05/What-Is-Kimchi-CO180-FE.jpg",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "coffee",
-    image: "https://cdn.zmescience.com/wp-content/uploads/2018/04/Cappuccino_at_Sightglass_Coffee-1.jpg",
-    rating: 5
-  },
-  {
-    id: 3,
-    name: "Bread",
-    image: "https://www.thespruceeats.com/thmb/9n3YM4RVKSWyUWQ0JCl7BwHrl1U=/4288x2848/filters:fill(auto,1)/loaf-of-bread-182835505-58a7008c5f9b58a3c91c9a14.jpg",
-    rating: 5
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
   }
-];
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}></Food>
-      ))}
-    </div>
-  );
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('update');
+  }
+
+  componentWillUnmount() {
+    console.log("unmounted")
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }))
+    console.log('add');
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }))
+    console.log('minus')
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
 
 export default App;
